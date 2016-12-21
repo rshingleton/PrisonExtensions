d1452a86020b7853863d3684c15d20bb96381441
Russell Shingleton <russellshomes@aol.com>
2016-12-20 23:21:13 -0500

    Initial Commit
    Rimworld prison extensions

---
+++ Source/PrisonExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("PrisonExtensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrisonExtensions")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("04035174-7FEF-4C9B-96F0-EF1DBF37378E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/PrisonExtensions/ThoughtWorker_SharedBedroom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace PrisonExtensions
+{
+    public class ThoughtWorker_SharedBedroom : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (p.IsPrisoner || p.IsPrisonerOfColony)
+            {
+                Log.Message("You're a prisoner, stop crying");
+                return false;
+            }
+            else
+            {
+                return p.ownership.OwnedBed != null && p.ownership.OwnedRoom == null &&
+                       !p.ownership.OwnedBed.GetRoom().PsychologicallyOutdoors;
+            }
+
+        }
+    }
+}
